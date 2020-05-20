326209ce5d9c45cd3a75f85f916d0c3c8d909b95
Vincent Liu <vincentliu99999@gmail.com>
2020-05-20 22:05:22 +0800

    move to another folder

--- src/main/java/com/vincent/practice/controller/HelloController.java
+++ src/main/java/com/vincent/practice/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.vincent.practice;
+package com.vincent.practice.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
