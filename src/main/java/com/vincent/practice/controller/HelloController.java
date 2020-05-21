d325b1ec6047da9b277f64ec4ccc669d3128a575
Vincent Liu <vincentliu99999@gmail.com>
2020-05-21 12:07:11 +0800

    add fail test

--- src/main/java/com/vincent/practice/controller/HelloController.java
+++ src/main/java/com/vincent/practice/controller/HelloController.java
@@ -1,14 +1,17 @@
 package com.vincent.practice.controller;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
-  @RequestMapping("/")
-  public String index() {
+  @RequestMapping("/")  public String index() {
     return "Greetings from Spring Boot!";
+  }
+
+  @RequestMapping("/hello")
+  public String hello() {
+    return "hello";
   }
 
 }
--- src/test/java/com/vincent/practice/TestingWebApplicationTest.java
+++ src/test/java/com/vincent/practice/TestingWebApplicationTest.java
@@ -5,9 +5,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,6 +21,12 @@ public class TestingWebApplicationTest {
   @Test
   public void shouldReturnDefaultMessage() throws Exception {
     this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+        .andExpect(content().string(containsString("Greetings from Spring Boot!")));
+  }
+
+  @Test
+  public void shouldReturnDefaultMessageHello() throws Exception {
+    this.mockMvc.perform(get("/hello")).andDo(print()).andExpect(status().isOk())
         .andExpect(content().string(containsString("Greetings from Spring Boot!")));
   }
 }
