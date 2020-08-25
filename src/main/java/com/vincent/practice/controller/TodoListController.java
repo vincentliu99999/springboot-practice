a873806fd0986a89e7b1ad63138e5dfd29cfd90c
Vincent Liu <vincentliu99999@gmail.com>
2020-08-26 00:28:33 +0800

    format

--- src/main/java/com/vincent/practice/controller/TodoListController.java
+++ src/main/java/com/vincent/practice/controller/TodoListController.java
@@ -81,86 +81,72 @@ public class TodoListController {
   }
 
   @GetMapping("/describeTable")
-  public String describeTable(@RequestParam(value = "name", defaultValue = "Todo") String tableName) {
+  public String describeTable(
+      @RequestParam(value = "name", defaultValue = "Todo") String tableName) {
 
-    DescribeTableRequest request = DescribeTableRequest.builder()
-            .tableName(tableName)
-            .build();
+    DescribeTableRequest request = DescribeTableRequest.builder().tableName(tableName).build();
 
     try {
-        TableDescription tableInfo =
-                ddb.describeTable(request).table();
+      TableDescription tableInfo = ddb.describeTable(request).table();
 
-        if (tableInfo != null) {
-            System.out.format("Table name  : %s\n",
-                    tableInfo.tableName());
-            System.out.format("Table ARN   : %s\n",
-                    tableInfo.tableArn());
-            System.out.format("Status      : %s\n",
-                    tableInfo.tableStatus());
-            System.out.format("Item count  : %d\n",
-                    tableInfo.itemCount().longValue());
-            System.out.format("Size (bytes): %d\n",
-                    tableInfo.tableSizeBytes().longValue());
+      if (tableInfo != null) {
+        System.out.format("Table name  : %s\n", tableInfo.tableName());
+        System.out.format("Table ARN   : %s\n", tableInfo.tableArn());
+        System.out.format("Status      : %s\n", tableInfo.tableStatus());
+        System.out.format("Item count  : %d\n", tableInfo.itemCount().longValue());
+        System.out.format("Size (bytes): %d\n", tableInfo.tableSizeBytes().longValue());
 
-            ProvisionedThroughputDescription throughputInfo =
-                    tableInfo.provisionedThroughput();
-            System.out.println("Throughput");
-            System.out.format("  Read Capacity : %d\n",
-                    throughputInfo.readCapacityUnits().longValue());
-            System.out.format("  Write Capacity: %d\n",
-                    throughputInfo.writeCapacityUnits().longValue());
+        ProvisionedThroughputDescription throughputInfo = tableInfo.provisionedThroughput();
+        System.out.println("Throughput");
+        System.out.format("  Read Capacity : %d\n", throughputInfo.readCapacityUnits().longValue());
+        System.out.format("  Write Capacity: %d\n",
+            throughputInfo.writeCapacityUnits().longValue());
 
-            List<AttributeDefinition> attributes =
-                    tableInfo.attributeDefinitions();
-            System.out.println("Attributes");
+        List<AttributeDefinition> attributes = tableInfo.attributeDefinitions();
+        System.out.println("Attributes");
 
-            for (AttributeDefinition a : attributes) {
-                System.out.format("  %s (%s)\n",
-                        a.attributeName(), a.attributeType());
-            }
+        for (AttributeDefinition a : attributes) {
+          System.out.format("  %s (%s)\n", a.attributeName(), a.attributeType());
         }
+      }
     } catch (DynamoDbException e) {
-        System.err.println(e.getMessage());
-        return e.getMessage();
+      System.err.println(e.getMessage());
+      return e.getMessage();
     }
     // snippet-end:[dynamodb.java2.describe_table.main]
     System.out.println("\nDone!");
     return "describeTable";
-}
+  }
 
-@PostMapping("/addTodo")
-public String addTodo(@RequestParam(value = "pk", defaultValue = "pk") String pk
-, @RequestParam(value = "sk", defaultValue = "sk") String sk) {
-  String tableName = "Todo";
+  @PostMapping("/addTodo")
+  public String addTodo(@RequestParam(value = "pk", defaultValue = "pk") String pk,
+      @RequestParam(value = "sk", defaultValue = "sk") String sk) {
+    String tableName = "Todo";
 
-  HashMap<String,AttributeValue> itemValues = new HashMap<String,AttributeValue>();
+    HashMap<String, AttributeValue> itemValues = new HashMap<String, AttributeValue>();
 
-  // Add all content to the table
-  itemValues.put("pk", AttributeValue.builder().s(pk).build());
-  itemValues.put("sk", AttributeValue.builder().s(sk).build());
-  itemValues.put("todo", AttributeValue.builder().s("todo2").build());
+    // Add all content to the table
+    itemValues.put("pk", AttributeValue.builder().s(pk).build());
+    itemValues.put("sk", AttributeValue.builder().s(sk).build());
+    itemValues.put("todo", AttributeValue.builder().s("todo2").build());
 
-  // Create a PutItemRequest object
-  PutItemRequest request = PutItemRequest.builder()
-          .tableName(tableName)
-          .item(itemValues)
-          .build();
+    // Create a PutItemRequest object
+    PutItemRequest request = PutItemRequest.builder().tableName(tableName).item(itemValues).build();
 
-  try {
+    try {
       ddb.putItem(request);
-      System.out.println(tableName +" was successfully updated");
+      System.out.println(tableName + " was successfully updated");
 
-  } catch (ResourceNotFoundException e) {
+    } catch (ResourceNotFoundException e) {
       System.err.format("Error: The table \"%s\" can't be found.\n", tableName);
       System.err.println("Be sure that it exists and that you've typed its name correctly!");
       return e.getMessage();
-  } catch (DynamoDbException e) {
+    } catch (DynamoDbException e) {
       System.err.println(e.getMessage());
       return e.getMessage();
-  }
+    }
 
-  return "xxx";
-}
+    return "xxx";
+  }
 
 }
