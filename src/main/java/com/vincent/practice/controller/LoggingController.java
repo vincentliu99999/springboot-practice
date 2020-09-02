48ed7f97d20e242f5882afc39541a7daa723de8d
Vincent Liu <vincentliu99999@gmail.com>
2020-09-02 21:16:56 +0800

    add logback config and sample controller

---
+++ src/main/java/com/vincent/practice/controller/LoggingController.java
@@ -0,0 +1,28 @@
+package com.vincent.practice.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/logging")
+/**
+ * Automatically configuring logback
+ */
+public class LoggingController {
+ 
+  private static final Logger logger = LoggerFactory.getLogger(LoggingController.class);
+ 
+    @PostMapping("/log")
+    public String index() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+ 
+        return "Howdy! Check out the Logs to see the output...";
+    }
+}
