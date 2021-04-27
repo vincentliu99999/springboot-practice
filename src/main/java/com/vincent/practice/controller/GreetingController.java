24a480b0d1b60cd3d9077778ae14f023bce92d77
Vincent Liu <vincentliu99999@gmail.com>
2021-04-27 13:39:04 +0800

    validate PersonForm

--- src/main/java/com/vincent/practice/controller/GreetingController.java
+++ src/main/java/com/vincent/practice/controller/GreetingController.java
@@ -1,8 +1,12 @@
 package com.vincent.practice.controller;
 
 import com.vincent.practice.model.Greeting;
+import com.vincent.practice.model.PersonForm;
+import io.swagger.annotations.ApiParam;
 import java.util.concurrent.atomic.AtomicLong;
+import javax.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +18,11 @@ public class GreetingController {
   @GetMapping("/greeting")
   public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
     return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
+  }
+
+  @PostMapping("/greetingForm")
+  public PersonForm greeting(
+      @ApiParam(value = "PersonForm", required = true) @Valid PersonForm person) {
+    return person;
   }
 }
