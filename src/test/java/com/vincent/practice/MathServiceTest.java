9297e33a96777f7beaa5b375d40ca0f43bbc6156
Vincent Liu <vincentliu99999@gmail.com>
2021-06-01 00:42:14 +0800

    add MathService unit test for add

---
+++ src/test/java/com/vincent/practice/MathServiceTest.java
@@ -0,0 +1,19 @@
+package com.vincent.practice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.vincent.practice.employee.MathService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = PracticeApplication.class)
+public class MathServiceTest {
+  @Autowired private MathService mathService;
+
+  @Test
+  public void add() {
+    int result = mathService.add(1, 2);
+    assertEquals(3, result);
+  }
+}
