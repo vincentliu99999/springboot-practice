553fb8b36b57a6d319651fba8e87b4108fb0327f
Vincent Liu <vincentliu99999@gmail.com>
2021-06-24 00:36:08 +0800

    move file

---
+++ src/main/java/com/vincent/practice/validation/ProductValidation.java
@@ -0,0 +1,20 @@
+package com.vincent.practice.validation;
+
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+public class ProductValidation {
+  public static void main(String[] args) {
+    Product product = new Product();
+
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    Validator validator = factory.getValidator();
+    Set<ConstraintViolation<Product>> violations = validator.validate(product);
+    for (ConstraintViolation<Product> violation : violations) {
+      System.out.println(violation.getMessage());
+    }
+  }
+}
