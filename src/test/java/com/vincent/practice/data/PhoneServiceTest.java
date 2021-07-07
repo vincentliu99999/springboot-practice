bd39f1a7647a2efc3a145d963f671efe039d5355
Vincent Liu <vincentliu99999@gmail.com>
2021-07-07 21:20:22 +0800

    add DynamoDbAsyncClient local test

---
+++ src/test/java/com/vincent/practice/data/PhoneServiceTest.java
@@ -0,0 +1,143 @@
+package com.vincent.practice.data;
+
+import com.amazonaws.services.dynamodbv2.local.main.ServerRunner;
+import com.amazonaws.services.dynamodbv2.local.server.DynamoDBProxyServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.Publisher;
+import org.w3c.dom.Attr;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
+import software.amazon.awssdk.services.dynamodb.model.*;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PhoneServiceTest {
+
+    public static final String COMPANY = "Company";
+    public static final String MODEL = "Model";
+    private static DynamoDBProxyServer dynamoProxy;
+
+    private static DynamoDbAsyncClient dynamoDbAsyncClient;
+
+    private static int port;
+    public static final String COLOR = "Color";
+    public static final String YEAR = "Year";
+
+    private static int getFreePort() {
+        try {
+            ServerSocket socket = new ServerSocket(0);
+            int port = socket.getLocalPort();
+            socket.close();
+            return port;
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+    }
+
+    @BeforeAll
+    public static void setupDynamo() {
+        System.setProperty("sqlite4java.library.path", "src/test/resources/libs/");
+        
+        port = getFreePort();
+        try {
+            dynamoProxy = ServerRunner.createServerFromCommandLineArgs(new String[]{
+                    "-inMemory",
+                    "-port",
+                    Integer.toString(port)
+            });
+            dynamoProxy.start();
+
+            dynamoDbAsyncClient = getDynamoClient();
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
+
+    @AfterAll
+    public static void teardownDynamo() {
+        try {
+            dynamoProxy.stop();
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
+
+    @Test
+    public void testStuff() throws Exception {
+        ListTablesResponse listTablesResponse = dynamoDbAsyncClient.listTables().get();
+
+        int totalTablesBeforeCreation = listTablesResponse.tableNames().size();
+
+        createTableAsync("Phones").get();
+
+        ListTablesResponse listTablesResponseAfterCreation = dynamoDbAsyncClient.listTables().get();
+
+        assertThat(listTablesResponseAfterCreation.tableNames().size()).isEqualTo(totalTablesBeforeCreation + 1);
+    }
+
+    private static DynamoDbAsyncClient getDynamoClient() {
+        return DynamoDbAsyncClient.builder()
+                .region(Region.US_EAST_1)
+                .endpointOverride(URI.create("http://localhost:" + port))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create("FAKE", "FAKE")))
+                .build();
+    }
+
+    private CompletableFuture<CreateTableResponse> createTableAsync(String tableName) {
+        return dynamoDbAsyncClient.createTable(CreateTableRequest.builder()
+                .keySchema(
+                        KeySchemaElement.builder()
+                                .keyType(KeyType.HASH)
+                                .attributeName(COMPANY)
+                                .build(),
+                        KeySchemaElement.builder()
+                                .keyType(KeyType.RANGE)
+                                .attributeName(MODEL)
+                                .build()
+                )
+                .attributeDefinitions(
+                        AttributeDefinition.builder()
+                                .attributeName(COMPANY)
+                                .attributeType(ScalarAttributeType.S)
+                                .build(),
+                        AttributeDefinition.builder()
+                                .attributeName(MODEL)
+                                .attributeType(ScalarAttributeType.S)
+                                .build()
+                )
+                .provisionedThroughput(ProvisionedThroughput.builder().readCapacityUnits(100L).writeCapacityUnits(100L).build())
+                .tableName(tableName)
+                .build()
+        );
+    }
+
+    private AttributeValue s(String value) {
+        return AttributeValue.builder().s(value).build();
+    }
+
+    private Map<String, AttributeValue> getMapWith(String companyName, String modelName) {
+        Map<String, AttributeValue> map = new HashMap<>();
+
+        map.put(COMPANY, AttributeValue.builder().s(companyName).build());
+        map.put(MODEL, AttributeValue.builder().s(modelName).build());
+
+        return map;
+    }
+}
