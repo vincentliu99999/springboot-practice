ea72e02a8b95687c3ea5c36697f810bea6cc16e8
Vincent Liu <vincentliu99999@gmail.com>
2021-07-10 17:46:27 +0800

    test add DynamoDBConfig

---
+++ src/test/java/com/vincent/practice/config/DynamoDBConfig.java
@@ -0,0 +1,29 @@
+package com.vincent.practice.config;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+@Configuration
+@ConfigurationProperties
+public class DynamoDBConfig {
+
+  @Value("${local-db-use}")
+  private boolean useLocalDB;
+
+  @Bean
+  @Scope("prototype")
+  public DynamoDbClient buildDDB() throws URISyntaxException {
+    return DynamoDbClient.builder().endpointOverride(new URI("http://localhost:8000")).build();
+    // if (useLocalDB)
+    //   return DynamoDbClient.builder().endpointOverride(new URI("http://localhost:8000")).build();
+    // else
+    //   return DynamoDbClient.builder().region(Region.US_EAST_1).build();
+  }
+}
